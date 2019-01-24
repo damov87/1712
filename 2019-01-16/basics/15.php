3c4d2ec627a60a92a535baf9144c1d362c5c3aa1
damov <damov87@gmail.com>
2019-01-24 15:20:48 +0200

    homework 2019-01-16 improvement

--- 2019-01-16/basics/15.php
+++ 2019-01-16/basics/15.php
@@ -1,84 +1,28 @@
 <?php
+include '15.html';
+$a = $_POST ['a'];
+$b = $_POST ['b'];
+$operator = $_POST ['operator'];
 
-$result = "";
-class calculator
-{
-    var $a;
-    var $b;
-
-    function checkopration($operator)
-    {
-        switch($operator)
-        {
-            case '+':
-            return $this->a + $this->b;
-            break;
-
-            case '-':
-            return $this->a - $this->b;
-            break;
-
-            case '*':
-            return $this->a * $this->b;
-            break;
-
-            case '/':
-            return $this->a / $this->b;
-            break;
-
-            case '%':
-            return $this->a % $this->b;
-            break;
-
-            default:
-            return "Sorry No command found";
-        }   
-    }
-    function getresult($a, $b, $c)
-    {
-        $this->a = $a;
-        $this->b = $b;
-        return $this->checkopration($c);
-    }
+if ($b == 0 && ($operator == '/' || $operator == '%')){
+    echo "Division by 0 is impossible!";
 }
-
-$cal = new calculator();
-if(isset($_POST['submit']))
-{   
-    $result = $cal->getresult($_POST['n1'],$_POST['n2'],$_POST['op']);
+else {
+    if ($operator == '+') {
+        $result = $a + $b; 
+    }
+    if ($operator == '-') {
+        $result = $a - $b;
+    }
+    if ($operator == '/')  {
+        $result = $a / $b;
+    }
+    if ($operator == '*') {
+        $result = $a * $b;
+    }
+    if ($operator == '%') {
+        $result = $a % $b;
+    }
+    echo 'Result: ' . $result;
 }
 ?>
-
-<form method="post">
-<table align="center">
-    <tr>
-        <td><strong><?php echo $result; ?><strong></td>
-    </tr>
-    <tr>
-        <td>1st Number</td>
-        <td><input type="text" name="n1"></td>
-    </tr>
-
-    <tr>
-        <td>2nd Number</td>
-        <td><input type="text" name="n2"></td>
-    </tr>
-
-    <tr>
-        <td>Operator</td>
-        <td><select name="op">
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">*</option>
-            <option value="/">/</option>
-            <option value="%">%</option>
-        </select></td>
-    </tr>
-
-    <tr>
-        <td></td>
-        <td><input type="submit" name="submit" value="                =                "></td>
-    </tr>
-
-</table>
-</form>

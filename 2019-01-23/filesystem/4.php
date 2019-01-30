2a312fead93a57e35c29120f3d5d64ead18d33cf
damov <damov87@gmail.com>
2019-01-30 16:39:43 +0200

    filesystem 4 50/50

--- 2019-01-23/filesystem/4.php
+++ 2019-01-23/filesystem/4.php
@@ -1,16 +1,45 @@
 <?php
 
-// function filesInDirectoryWithTypes($scan)
-// {
-// 		$files = scandir($scan);
-// 		usort($files, function ($a, $b) {
-// 	    	return mb_strlen($a, 'UTF-8') - mb_strlen($b, 'UTF-8');
-// 		});
-// 		foreach($files as $files){
-// 		   echo '<li>'.$files.'</li>';
-// 		}
-// }
+function filesInDirectoryWithTypes($scan)
+{
+		$files = scandir($scan);
+		$directories = [];
+	    $files_list  = [];
 
-// echo '<pre>';
-// print_r(filesInDirectoryWithTypes('../..'));
-// echo '</pre>';
+		foreach ($files as $file) {
+		      if (is_dir($scan . '/' . $file)) {
+		         $directories[] = $file;
+		      } else {
+		         $files_list[] = $file;
+		      }
+		}
+
+		foreach ($files_list as $key => $value) {
+			$myPathInfo = pathinfo($value);
+			if (isset($myPathInfo['extension']) === true) {
+				$ext[] = $myPathInfo['extension'];
+			}
+		}
+
+		echo '<pre>';
+		print_r($ext);
+		echo '</pre>';
+
+		$extCount = array_count_values($ext);
+		arsort($extCount);
+
+		echo '<pre>';
+		print_r($extCount);
+		echo '</pre>';
+
+		foreach($directories as $directory){
+		   echo '<li type="circle">'.$directory.'</li>';
+		}
+		foreach($files_list as $file_list){
+		   echo '<li>'.$file_list.'</li>';
+		}
+}
+
+echo '<pre>';
+print_r(filesInDirectoryWithTypes('../..'));
+echo '</pre>';
